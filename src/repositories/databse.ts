9ff7baccb5a069998653725203bcf9bc76b34b57
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-06-02 19:00:45 +0900

    fix: asyncに変更
    Webから取得するための変更

--- src/repositories/databse.ts
+++ src/repositories/databse.ts
@@ -5,7 +5,7 @@ import { SaveData } from "../models/saveData";
  * ログインした情報からゲーム側のセーブデータを取得し、構造化する関数
  * @returns saveData
  */
-export function getSaveData(): SaveData {
+export async function getSaveData(): SaveData {
     // TODO: ゲーム側のセーブデータを取得するようにする。
     const result = TEST_SAVE_DATA;
     return result;
