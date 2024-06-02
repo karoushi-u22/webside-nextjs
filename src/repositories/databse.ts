da14743c6dff1d1dbc2f132bf85b142eef5a5f27
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-06-02 19:02:11 +0900

    fix: Promise<SaveData>を返すよう変更

--- src/repositories/databse.ts
+++ src/repositories/databse.ts
@@ -5,7 +5,7 @@ import { SaveData } from "../models/saveData";
  * ログインした情報からゲーム側のセーブデータを取得し、構造化する関数
  * @returns saveData
  */
-export async function getSaveData(): SaveData {
+export async function getSaveData(): Promise<SaveData> {
     // TODO: ゲーム側のセーブデータを取得するようにする。
     const result = TEST_SAVE_DATA;
     return result;
