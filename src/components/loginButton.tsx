d3a2ffb9fcdd958bd7841f736f836fe0ebd9a90b
96nekocake <96nekocake@gmail.com>
2024-06-18 16:33:16 +0900

    fix:コンポーネントの名前を変更
    フォルダ名をcomponentsに変更。ファイル名、関数名をloginButtonに統一しました

--- src/components/loginButton.tsx
+++ src/components/loginButton.tsx
@@ -9,6 +9,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSteam } from '@fortawesome/free-brands-svg-icons';
 import '../app/globals.css'; 
 
-export function SignIn() {
-  return <button onClick={() => signIn('steam')} class="flex items-center px-4 py-2 bg-[#231f20] text-[#fff] rounded shadow-md hover:bg-[#2e2a2b]"><FontAwesomeIcon icon={faSteam} className="mr-2 w-5 h-5" />Steamでログイン</button>;  
-}
+const LoginButton = () => {
+  return (
+    <button
+      onClick={() => signIn('steam')}
+      className="flex items-center px-4 py-2 bg-[#231f20] text-[#fff] rounded shadow-md hover:bg-[#2e2a2b]">
+      <FontAwesomeIcon icon={faSteam} className="mr-2 w-5 h-5" />
+      Steamでログイン
+    </button>
+  );
+};
+
+export default LoginButton;
